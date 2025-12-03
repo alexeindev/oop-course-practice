a858f32a7fd837456562fa3d173a7dae11534e82
Alejandra Lopez <alejandralopez8212@gmail.com>
2025-12-03 08:34:20 -0500

    Add Selenium dependency and Encapsulation POM example

--- src/main/java/Encapsulation/POMExample/POMExample.java
+++ src/main/java/Encapsulation/POMExample/POMExample.java
@@ -5,15 +5,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class POMExample {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
-
         try {
+            // ENCAPSULATION: The test doesn't know:
+            // - What locators are used (id, xpath, css?)
+            // - How many steps the login takes
+            // - If there are waits or validations inside
+            // - If there is a change in the login functionality, the test won't change
+            // It only knows the PUBLIC interface: login(user, pass) - Only knows how to interact
             driver.get("https://practicetestautomation.com/practice-test-login/");
-
             LoginPage loginPage = new LoginPage(driver);
-            loginPage.login("student", "Password123");  // Aún más limpio
-
-            Thread.sleep(2000);
-
+            loginPage.login("student", "Password123");
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
